25dd36d42cffd44c606f09b0158afeb6649b04d1
Sodiq Alase <sodiqalase@yahoo.com>
2020-07-19 13:48:10 +0100

    fixing issues

---
+++ build/precache-manifest.cd96a4951b212abcf7379d375d4fb412.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35c034d82a2972eb3420d28bb71dee5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "895d8f81d69680f8b412",
+    "url": "/static/css/main.25207477.chunk.css"
+  },
+  {
+    "revision": "ed5d52d0592f1229feaf",
+    "url": "/static/js/2.998eb9f0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.998eb9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895d8f81d69680f8b412",
+    "url": "/static/js/main.b844286d.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "0795bf175660deab14e694627ff4e294",
+    "url": "/static/media/Clip.0795bf17.woff"
+  },
+  {
+    "revision": "695d4f63a5888639217a43e401851974",
+    "url": "/static/media/Kopeek20-Regular.695d4f63.woff"
+  },
+  {
+    "revision": "243f566d3815ac82cd19e0ed6ce6fdeb",
+    "url": "/static/media/MonumentExtended-Regular.243f566d.woff2"
+  },
+  {
+    "revision": "5cc3087dc0b0a75ff38e0ba32c185c3e",
+    "url": "/static/media/MonumentExtended-Ultrabold.5cc3087d.woff2"
+  },
+  {
+    "revision": "696ade59738d632297e12db9905c7707",
+    "url": "/static/media/Neoneon-3zaD6.696ade59.otf"
+  },
+  {
+    "revision": "6f8598d7206d33ea4dbaadc077241a54",
+    "url": "/static/media/Rhone.6f8598d7.png"
+  },
+  {
+    "revision": "bdfd8076c919de9ec00352d384740fbf",
+    "url": "/static/media/aptitude.bdfd8076.png"
+  },
+  {
+    "revision": "41f1f67ef8764123476127c826726687",
+    "url": "/static/media/babel.41f1f67e.png"
+  },
+  {
+    "revision": "5bf23194333f8648968e26f15a67a395",
+    "url": "/static/media/blur-close-up-code-computer-546819.5bf23194.jpg"
+  },
+  {
+    "revision": "4fe2604ea29c7dad0c29344495434b8f",
+    "url": "/static/media/coolmovies.4fe2604e.png"
+  },
+  {
+    "revision": "e19ad2c3048efb5546ca41a46bdaafa7",
+    "url": "/static/media/hlp.e19ad2c3.png"
+  },
+  {
+    "revision": "d79d40e3e46761dbaf12b52d5aa1bc52",
+    "url": "/static/media/jest.d79d40e3.png"
+  },
+  {
+    "revision": "c04cf9200b6c86c9593e73c9640243ee",
+    "url": "/static/media/movie.c04cf920.png"
+  },
+  {
+    "revision": "bf3efff2d50f5a3b2c0bcee8c268fcf2",
+    "url": "/static/media/netlify.bf3efff2.png"
+  },
+  {
+    "revision": "290659dcfc256514f26642d9d7f62de0",
+    "url": "/static/media/portfolio.290659dc.png"
+  },
+  {
+    "revision": "cff407049dba3ded8423ec32d057e83a",
+    "url": "/static/media/react (2).cff40704.png"
+  },
+  {
+    "revision": "ffb5427108cc70227db56501edbd10fb",
+    "url": "/static/media/salestracker.ffb54271.png"
+  },
+  {
+    "revision": "b186d41c7b568acae1a58986f91f81ad",
+    "url": "/static/media/shoepro.b186d41c.png"
+  },
+  {
+    "revision": "959aa4adc7f7a3a91d5663b0b9a27c34",
+    "url": "/static/media/sodiq5.959aa4ad.jpg"
+  },
+  {
+    "revision": "ca4a1034d50aa463b1d9127754fcaac0",
+    "url": "/static/media/wordpress.ca4a1034.png"
+  }
+]);
